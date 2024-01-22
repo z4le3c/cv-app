cbfb427d30bd75f8e59c080fa06a848ee68497f9
zalec <142818531+z4le3c@users.noreply.github.com>
2024-01-22 17:09:28 -0600

    fix  general info section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,38 @@ import { useState } from "react"
 import GeneralInfoSection from "./components/GeneralInfoSection"
 import EducationInfoSection from "./components/EducationInfoSection"
 import ExperienceInfoSection from "./components/ExperienceInfoSection"
-import CVOutput from "./components/CVOutput"
 
 import "./App.css"
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  })
+
+  const [educationalInfo, setEducationalInfo] = useState([
+    {school:'School1', id:1},
+    {school:'School2', id:2},
+  ])
+  const [practicalInfo, setPracticalInfo] = useState([])
+
   return (
     <>
-      <div className="edit-container">
-        <GeneralInfoSection ></GeneralInfoSection >
-        <EducationInfoSection></EducationInfoSection>
-        <ExperienceInfoSection></ExperienceInfoSection>
+      <div className="cv-container">
+        <GeneralInfoSection
+          generalInfo={generalInfo}
+          setGeneralInfo={setGeneralInfo}
+        ></GeneralInfoSection>
+        <EducationInfoSection
+          educationalInfo={educationalInfo}
+          setEducationalInfo={setEducationalInfo}
+        ></EducationInfoSection>
+        <ExperienceInfoSection
+          practicalInfo={practicalInfo}
+          setPracticalInfo={setPracticalInfo}
+        ></ExperienceInfoSection>
       </div>
-      <CVOutput></CVOutput>
     </>
   )
 }
--- src/components/GeneralInfoSection.jsx
+++ src/components/GeneralInfoSection.jsx
@@ -1,74 +1,73 @@
 import React, { useState } from "react"
 
-function Input({ id, labelText, disabled, onChange, value }) {
-  return (
-    <div className="input-container">
-      <label htmlFor={id} className="input-label">
-        {labelText}
-      </label>
-      <input
-        id={id}
-        type="text"
-        value={value}
-        onChange={onChange}
-        disabled={disabled}
-        placeholder={labelText}
-      />
-    </div>
-  )
-}
-
-function GeneralInfoSection() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [phone, setPhone] = useState("")
-  const [isEditing, setIsEditing] = useState(true)
+function GeneralInfoSection({generalInfo, setGeneralInfo}) {
+  const [editing, setEditing] = useState(true)
 
   const handleEdit = () => {
-    setIsEditing(true)
+    setEditing(true)
   }
 
-  const handleSave = (e) => {
-    e.preventDefault()
-    // Handle form submission logic here
-    console.log("Submitted:", { name, email, phone })
-    setIsEditing(false)
+  const handleSave = () => {
+    setEditing(false)
   }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setGeneralInfo({
+      ...generalInfo,
+      [name]: value,
+    });
+  };
 
   return (
-    <div className="general-info edit-card">
-      <h1>General Info</h1>
-      <Input
-        id="name"
-        labelText="Name"
-        disabled={!isEditing}
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      ></Input>
-      <Input
-        id="email"
-        labelText="Email"
-        disabled={!isEditing}
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      ></Input>
-      <Input
-        id="phone"
-        labelText="Phone Number"
-        disabled={!isEditing}
-        onChange={(e) => setPhone(e.target.value)}
-        value={phone}
-      ></Input>
-
-      {isEditing ? (
-        <button type="button" onClick={handleSave}>
-          Save
-        </button>
-      ) : (
-        <button type="button" onClick={handleEdit}>
-          Edit
-        </button>
-      )}
+    <div className="general-info section" onClick={!editing ? handleEdit : undefined}>
+      <h2>General Info</h2>
+      {
+        editing ? (
+          <>
+            <div className="input-container">
+              <label htmlFor="name" className="input-label">Name:</label>
+              <input 
+                type="text" 
+                id="name"
+                name="name"
+                value={generalInfo.name}
+                onChange={handleChange}
+                placeholder="fuil name"
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="email" className="input-label">Email:</label>
+              <input 
+                type="text" 
+                id="email"
+                name="email"
+                value={generalInfo.email}
+                onChange={handleChange}
+                placeholder="email"
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="phone" className="input-label">Phone:</label>
+              <input 
+                type="text" 
+                id="phone"
+                name="phone"
+                value={generalInfo.phone}
+                onChange={handleChange}
+                placeholder="phone number"
+              />
+            </div>
+            <button onClick={handleSave}>Save</button>
+          </>
+        ) : (
+          <div>
+            <p>Name: {generalInfo.name}</p>
+            <p>Email: {generalInfo.email}</p>
+            <p>Phone: {generalInfo.phone}</p>
+          </div>
+        )
+      }
     </div>
   )
 }
